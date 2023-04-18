b9893dda62a3d859106dc67930be57039fe1a880
key <30455795+keyhsw@users.noreply.github.com>
2023-04-18 10:03:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,87 @@
-import gradio    
-from gradio.core.schemas import MediaType    
-from gradio.frontends.web import WebFrontend    
-from gradio.frontends.web.inputs import WebCameraInput    
-from gradio.frontends.web.outputs import WebVideoOutput    
-from flask import Flask, request, render_template
+import gradio as gr 
+import pandas as pd 
+from skimage import data
+from PIL import Image
+from torchkeras import plots 
+from torchkeras.data import get_url_img
+from pathlib import Path
+from ultralytics import YOLO
+import ultralytics
+from ultralytics.yolo.data import utils 
 
-app = Flask(__name__)
+model = YOLO('yolov8n.pt')
 
-# 创建 WebFrontend 对象并添加摄像头输入和输出    
-frontend = WebFrontend(url_prefix='/video', MediaType.VIDEO)    
-frontend.add_input(WebCameraInput(prompt='Pick a camera'))    
-frontend.add_output(WebVideoOutput())
+#prepare example images
+Image.fromarray(data.coffee()).save('coffee.jpeg') 
+Image.fromarray(data.astronaut()).save('people.jpeg')
+Image.fromarray(data.cat()).save('cat.jpeg')
 
-# 注册路由以处理视频流请求    
-@app.route('/', methods=['GET', 'POST'])    
-def video_stream():    
-    if request.method == 'POST':    
-        # 获取摄像头输入的选择    
-        camera_index = request.form.get('camera_index')    
-        frontend.set_input(frontend.inputs[camera_index])    
-        # 打开摄像头并获取视频流    
-        stream = frontend.start_stream()    
-        return stream    
-    else:    
-        # 默认 GET 请求，显示摄像头图标    
-        return render_template('video.html')
+#load class_names
+yaml_path = str(Path(ultralytics.__file__).parent/'datasets/coco128.yaml') 
+class_names = utils.yaml_load(yaml_path)['names']
 
-if __name__ == '__main__':    
-    app.run()    
+def detect(img):
+    if isinstance(img,str):
+        img = get_url_img(img) if img.startswith('http') else Image.open(img).convert('RGB')
+    result = model.predict(source=img)
+    if len(result[0].boxes.boxes)>0:
+        vis = plots.plot_detection(img,boxes=result[0].boxes.boxes,
+                     class_names=class_names, min_score=0.2)
+    else:
+        vis = img
+    return vis
+
+with gr.Blocks() as demo:
+    gr.Markdown("# yolov8目标检测演示")
+
+    with gr.Tab("捕捉摄像头喔"):
+        in_img = gr.Image(source='webcam',type='pil')
+        button = gr.Button("执行检测",variant="primary")
+
+        gr.Markdown("## 预测输出")
+        out_img = gr.Image(type='pil')
+
+        button.click(detect,
+                     inputs=in_img, 
+                     outputs=out_img)
+        
+    
+    with gr.Tab("选择测试图片"):
+        files = ['people.jpeg','coffee.jpeg','cat.jpeg']
+        drop_down = gr.Dropdown(choices=files,value=files[0])
+        button = gr.Button("执行检测",variant="primary")
+        
+        
+        gr.Markdown("## 预测输出")
+        out_img = gr.Image(type='pil')
+        
+        button.click(detect,
+                     inputs=drop_down, 
+                     outputs=out_img)
+        
+    with gr.Tab("输入图片链接"):
+        default_url = 'https://t7.baidu.com/it/u=3601447414,1764260638&fm=193&f=GIF'
+        url = gr.Textbox(value=default_url)
+        button = gr.Button("执行检测",variant="primary")
+        
+        gr.Markdown("## 预测输出")
+        out_img = gr.Image(type='pil')
+        
+        button.click(detect,
+                     inputs=url, 
+                     outputs=out_img)
+        
+    with gr.Tab("上传本地图片"):
+        input_img = gr.Image(type='pil')
+        button = gr.Button("执行检测",variant="primary")
+        
+        gr.Markdown("## 预测输出")
+        out_img = gr.Image(type='pil')
+        
+        button.click(detect,
+                     inputs=input_img, 
+                     outputs=out_img)
+        
+gr.close_all() 
+demo.queue(concurrency_count=5)
+demo.launch()
